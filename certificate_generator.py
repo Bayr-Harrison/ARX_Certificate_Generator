ef020f4b5553e0e3f7372aaaf09c15c67c0781e1
Bayr-Harrison <121176740+Bayr-Harrison@users.noreply.github.com>
2025-02-22 09:15:51 +0000

    Update certificate_generator.py

--- certificate_generator.py
+++ certificate_generator.py
@@ -97,8 +97,8 @@ with tabs[0]:  # Certificate Generator Page
             # Define fancy text placement
             name_id_text = f"{name} ({iatc_id})"
             text_font = "Times-Bold"
-            text_size = 50
-            date_font_size = 30
+            text_size = 30
+            date_font_size = 20
 
             # Calculate text width for centering
             text_width = fitz.get_text_length(name_id_text, fontsize=text_size, fontname=text_font)
@@ -109,7 +109,7 @@ with tabs[0]:  # Certificate Generator Page
             x_center_date = (page.rect.width - date_width) / 2
 
             page.insert_text((x_center_name, 300), name_id_text, fontsize=text_size, fontname=text_font, color=(0, 0, 0))
-            page.insert_text((x_center_date, 380), issue_date, fontsize=date_font_size, fontname=text_font, color=(0, 0, 0))
+            page.insert_text((x_center_date, 385), issue_date, fontsize=date_font_size, fontname=text_font, color=(0, 0, 0))
 
             pdf_buffer = io.BytesIO()
             doc.save(pdf_buffer)
